84fda03203d28e1cef88a82573fda1702ba591b4
Lifeng-Liang <lifeng.liang@gmail.com>
2015-05-19 19:14:17 +0800

    把单元测试改成NUnit。

--- Blog.UnitTests/TestPageList.cs
+++ Blog.UnitTests/TestPageList.cs
@@ -1,14 +1,14 @@
 ﻿using Leafing.Web;
 using Leafing.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using Blog.Biz.Models;
 using System;
 using System.Collections.Generic;
 
 namespace Blog.UnitTests
 {
-    [TestClass]
-    public class TestPageList : TestBase
+	[TestFixture]
+    public class TestPageList
     {
         private static long[] GetLinkNumList(ListStyle style, long pageCount, long pageIndex, long linkCount = 5)
         {
@@ -17,32 +17,32 @@ namespace Blog.UnitTests
             return list.ToArray();
         }
 
-        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void TestOutRange1()
         {
             GetLinkNumList(ListStyle.Default, 99, 100);
         }
 
-        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void TestOutRange2()
         {
             GetLinkNumList(ListStyle.Default, 99, -1);
         }
 
-        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [Test, ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void TestOutRange3()
         {
             GetLinkNumList(ListStyle.Default, 10, 5, -1);
         }
 
-        [TestMethod]
+        [Test]
         public void TestDefault1()
         {
             var list = GetLinkNumList(ListStyle.Default, 99, 7);
             Assert.AreEqual(new[] {1L, -1L, 6L, 7L, 8L, -1L, 99L}, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestDefault2()
         {
             var list = GetLinkNumList(ListStyle.Default, 99, 1);
@@ -53,14 +53,14 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 1L, 2L, 3L, 4L, -1L, 99L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestDefault3()
         {
             var list = GetLinkNumList(ListStyle.Default, 99, 4);
             Assert.AreEqual(new[] { 1L, -1L, 3L, 4L, 5L, -1L, 99L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestDefault4()
         {
             var list = GetLinkNumList(ListStyle.Default, 99, 97);
@@ -71,14 +71,14 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 1L, -1L, 96L, 97L, 98L, 99L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestDefault5()
         {
             var list = GetLinkNumList(ListStyle.Default, 99, 96);
             Assert.AreEqual(new[] { 1L, -1L, 95L, 96L, 97L, -1L, 99L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestDefault6()
         {
             var list = GetLinkNumList(ListStyle.Default, 5, 1);
@@ -89,7 +89,7 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 1L, 2L, 3L, 4L, 5L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestDefault7()
         {
             var list = GetLinkNumList(ListStyle.Default, 5, 1, 10);
@@ -100,7 +100,7 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 1L, 2L, 3L, 4L, 5L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestDefault8()
         {
             var list = GetLinkNumList(ListStyle.Default, 5, 1, 11);
@@ -115,14 +115,14 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestStatic1()
         {
             var list = GetLinkNumList(ListStyle.Static, 99, 58);
             Assert.AreEqual(new[] { 99L, -1L, 59L, 58L, 57L, -1L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestStatic2()
         {
             var list = GetLinkNumList(ListStyle.Static, 99, 99);
@@ -133,14 +133,14 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 99L, 98L, 97L, 96L, -1L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestStatic3()
         {
             var list = GetLinkNumList(ListStyle.Static, 99, 96);
             Assert.AreEqual(new[] { 99L, -1L, 97L, 96L, 95L, -1L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestStatic4()
         {
             var list = GetLinkNumList(ListStyle.Static, 99, 1);
@@ -151,14 +151,14 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 99L, -1L, 4L, 3L, 2L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestStatic5()
         {
             var list = GetLinkNumList(ListStyle.Static, 99, 4);
             Assert.AreEqual(new[] { 99L, -1L, 5L, 4L, 3L, -1L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestStatic6()
         {
             var list = GetLinkNumList(ListStyle.Static, 5, 5);
@@ -169,7 +169,7 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 5L, 4L, 3L, 2L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestStatic7()
         {
             var list = GetLinkNumList(ListStyle.Static, 5, 5, 10);
@@ -180,7 +180,7 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 5L, 4L, 3L, 2L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestStatic8()
         {
             var list = GetLinkNumList(ListStyle.Static, 5, 1, 11);
@@ -195,14 +195,14 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestHybird1()
         {
             var list = GetLinkNumList(ListStyle.Hybird, 100, 58);
             Assert.AreEqual(new[] { 0L, -1L, 59L, 58L, 57L, -1L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestHybird2()
         {
             var list = GetLinkNumList(ListStyle.Hybird, 100, 99);
@@ -213,14 +213,14 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 0L, 99L, 98L, 97L, -1L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestHybird3()
         {
             var list = GetLinkNumList(ListStyle.Hybird, 100, 97);
             Assert.AreEqual(new[] { 0L, -1L, 98L, 97L, 96L, -1L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestHybird4()
         {
             var list = GetLinkNumList(ListStyle.Hybird, 100, 1);
@@ -231,14 +231,14 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 0L, -1L, 4L, 3L, 2L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestHybird5()
         {
             var list = GetLinkNumList(ListStyle.Hybird, 100, 4);
             Assert.AreEqual(new[] { 0L, -1L, 5L, 4L, 3L, -1L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestHybird6()
         {
             var list = GetLinkNumList(ListStyle.Hybird, 5, 0);
@@ -249,7 +249,7 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 0L, 4L, 3L, 2L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestHybird7()
         {
             var list = GetLinkNumList(ListStyle.Hybird, 5, 0, 10);
@@ -260,7 +260,7 @@ namespace Blog.UnitTests
             Assert.AreEqual(new[] { 0L, 4L, 3L, 2L, 1L }, list);
         }
 
-        [TestMethod]
+        [Test]
         public void TestHybird8()
         {
             var list = GetLinkNumList(ListStyle.Hybird, 12, 0, 11);
